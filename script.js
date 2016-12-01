6b0cb0db25138ac5d99c4fdd68877f706a9cfacf
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-01 15:23:12 -0600

    added a div for where tree will go, connected button with event listener, connected character input field with character object

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
-var tree ={
-    
+// Tree Object
+
+var treeSpecs = {
+    height: 0,
+    character: '',
 }
+
+// Variable attatched to html elements
+
+var growTree = document.querySelector('#growtree');
+
+var treeHeight = document.querySelector('#tree-height');
+
+treeSpecs.character = document.querySelector('#character');
+
+// Event Listener on button
+
+growTree.addEventListener("click", function tree (myObject) {
+
+document.querySelector('#tree-spot').innerHTML = treeSpecs.character.value;
+
+
+
+});
+
+//  FUNCTIONS
+
+function treeLines() {
+
+
+   
+}
+
+// Main Button grow function
+
+
+
+
+
+
+// call function
+
+
+
+treeLines();
