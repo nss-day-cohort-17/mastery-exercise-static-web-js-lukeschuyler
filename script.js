c929eaa303423da4a861fb4b00fcf45e6449c840
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-02 10:03:54 -0600

    printed tree to console

--- script.js
+++ script.js
@@ -13,22 +13,32 @@ treeSpecs.character = document.querySelector('#character');
 
 // Event Listener on button
 
-growTree.addEventListener("click", function tree (myObject) {
-
-document.querySelector('#tree-spot').innerHTML = treeSpecs.character.value;
+growTree.addEventListener("click", function tree () {
 
 treeSpecs.height = document.querySelector('#tree-height').value;
 
-        for (var i = 0; i < treeSpecs.height; i += 2) {
+var treeSpot = document.querySelector('#tree-spot');
 
-            if (treeSpecs.height = 1)  {
+var numbOfSpaces = treeSpecs.height;
 
-                
+var numbOfChars = 1;
 
-            }
+        console.log("```\n");
 
-    }
+        for (var i = 0; i <= treeSpecs.height; i++ ) {
 
+            var blankSpace = ' '.repeat(numbOfSpaces);
+
+            var printChars = treeSpecs.character.value.repeat(numbOfChars)
+
+            console.log(blankSpace + printChars + blankSpace + '\n');
+
+             numbOfSpaces -= 1;
+
+             numbOfChars += 2;
+
+        }
+        console.log("```\n");
 });
 
 //  FUNCTIONS
@@ -43,10 +53,14 @@ function treeLines(lineAmount) {
 
 
 
-
-
 // call function
 
 
 
 treeLines();
+
+
+
+
+
+// " ``` <br>" + '\s'.repeat(numbOfSpaces) + treeSpecs.character.value.repeat(numbOfChars) + '\s'.repeat(numbOfSpaces) + '<br>';
